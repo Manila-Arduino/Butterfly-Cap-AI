fa887e7dd987054af89c1e9a3aaa247d8d4ee5a4
pyTimK <timkristianllanto.tk@gmail.com>
2025-06-04 23:38:32 +0800

    .

--- try.py
+++ try.py
@@ -2,13 +2,18 @@
 
 import board
 import busio
-from adafruit_ssd1306 import SSD1306_I2C
+
+# from adafruit_ssd1306 import SSD1306_I2C
+from adafruit_sh1106 import SH1106_I2C
+
 from PIL import Image, ImageDraw, ImageFont
 import time
 
 # Initialize I²C bus and SSD1306 OLED (128×64)
 i2c = busio.I2C(board.SCL, board.SDA)
-oled = SSD1306_I2C(128, 64, i2c)
+# oled = SSD1306_I2C(128, 64, i2c)
+oled = SH1106_I2C(132, 64, i2c, addr=0x3C)
+
 
 # Clear display
 oled.fill(0)
